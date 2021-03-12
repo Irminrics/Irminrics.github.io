bd39d1f1831a02ae5f9ef4e4886430a29703ef07
Irminrics <33411270+Irminrics@users.noreply.github.com>
2021-03-12 21:08:30 +0800

    Tiny Updates for Validations

--- js/homepage.js
+++ js/homepage.js
@@ -351,6 +351,15 @@ $(document).ready(function () {
     })
 
 
+    setInputFilter(document.getElementById("contactPhone"), function (value) {
+        return /^\d*\.?\d*$/.test(value); 
+    });
+
+    setInputFilter(document.getElementById("contactName"), function (value) {
+        return /^[A-Za-z]+$/.test(value); 
+    });
+
+
 });
 
 
@@ -403,6 +412,24 @@ $(document).scroll(function () {
     }
 });
 
+
+// Restricts input for the given textbox to the given inputFilter function.
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    });
+}
 
 
 // When the user scrolls the page, execute functions
